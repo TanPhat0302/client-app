ff7c7deddbfc122b7886a65d3c27cb65ca18ad3b
Phat Mai Nguyen Tan <phatt.maii01@gmail.com>
2023-10-10 12:06:49 +0300

    Build

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import { observer } from 'mobx-react-lite';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet, ScrollRestoration, useLocation } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import { ToastContainer } from 'react-toastify';
 import { useStore } from '../stores/store';
@@ -13,6 +13,7 @@ import ModalContainer from '../common/modals/ModalContainer';
 
 function App() {
   const location = useLocation();
+
   const {commonStore, userStore} = useStore();
 
   useEffect(()=> {
@@ -27,6 +28,7 @@ function App() {
 
   return (
     <>
+    <ScrollRestoration />
     <ModalContainer />
     <ToastContainer position='bottom-right' hideProgressBar theme='colored' />
       {location.pathname === '/' ? <HomePage />
--- src/index.tsx
+++ src/index.tsx
@@ -15,9 +15,8 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  //<React.StrictMode>
     <StoreContext.Provider value={store}>
     <RouterProvider router={router} />
     </StoreContext.Provider>
-  //</React.StrictMode>
+
 );
